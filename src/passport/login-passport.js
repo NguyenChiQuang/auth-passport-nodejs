fc41de76ca66030fd24a6b8de61f1bbef633c74a
hoc-anms <phamvanhoc96@gmail.com>
2018-12-11 18:23:52 +0700

    fix get user

--- src/passport/login-passport.js
+++ src/passport/login-passport.js
@@ -44,7 +44,7 @@ module.exports = new PassportLocalStrategy({
       };
 
       // create a token string
-      const token = jwt.sign(payload, config.JwtSecret);
+      const token = jwt.sign(payload, config.JwtSecret, {expiresIn: "1h"});
       const data = {
         name: user.email
       };
