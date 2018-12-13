f925382d2d873ba6bb10233dbdcad8dbfd3f2c2d
hoc-anms <phamvanhoc96@gmail.com>
2018-12-13 21:03:44 +0700

    update redirect route product

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,12 @@ const express = require('express')
 
 const user = require('./user');
 
+const product =require('./product')
+
 const router = express.Router()
 
 router.use('/auth', user);
+
+router.use('/auth',product);    
 
 module.exports = router;
